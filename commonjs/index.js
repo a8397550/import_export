1bd97093acd28400b937264ac399ee1c601c4379
lijunyang <275120399@qq.com>
2020-01-11 11:51:31 +0800

    首次提交

---
+++ commonjs/index.js
@@ -0,0 +1,5 @@
+const A = require('./main');
+const { a } = require('./main');
+
+console.log(A);
+console.log(a);
---
+++ commonjs/main.js
@@ -0,0 +1,8 @@
+exports.a = 1;
+exports.b = 2;
+exports.c = 3;
+
+// module.exports = {
+//   b: 2,
+//   c: 3
+// }
